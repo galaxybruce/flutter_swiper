e98ccb391146568d81837d37412b59395f75e66a
许国琛 <guochen.xu@linkkids.cn>
2020-04-01 17:32:20 +0800

    测试

--- lib/flutter_swiper.dart
+++ lib/flutter_swiper.dart
@@ -7,3 +7,5 @@ export 'src/swiper_controller.dart';
 export 'src/swiper_plugin.dart';
 export 'src/physics/LeftAlignScrollPhysics.dart';
 export 'src/physics/FirstRightEdgeAlignScrollPhysics.dart';
+
+
